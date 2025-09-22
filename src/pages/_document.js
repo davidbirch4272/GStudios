0c63b6f8d8761b1fe6c7caf5262398968ed81bf0
davidbirch4272 <davidbirch4272@gmail.com>
2025-09-22 13:02:25 -0400

    Changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,11 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+         <Head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>    
       <body>
         <Main />
         <NextScript />
